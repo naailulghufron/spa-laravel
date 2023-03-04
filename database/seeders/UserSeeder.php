cf4c38cabd4477d26a20f7d1e51153ec1b6880ec
naailulghufron <63755478+naailulghufron@users.noreply.github.com>
2023-03-05 00:40:25 +0700

    create 1000 user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class UserSeeder extends Seeder
 {
@@ -19,5 +20,11 @@ class UserSeeder extends Seeder
             'email' => 'admin@admin.com',
             'password' => bcrypt('123456'),
         ]);
+        // $f = Faker::create(100);
+        // User::create([
+        //     'name' => $f->name,
+        //     'email' => $f->email,
+        //     'password' => bcrypt('123456'),
+        // ]);
     }
 }
