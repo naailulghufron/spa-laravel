9c1a361efc04aed63f979238ee6b7b8777824379
cabinindo <app@cabinindo.co.id>
2023-03-09 14:30:48 +0700

    auth vue

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -12,60 +12,66 @@ import Profile from '../pages/users/Profile.vue'
 import Register from '../pages/users/Register.vue'
 import EditUser from '../pages/users/Edit.vue'
 import UploadImageUser from '../pages/users/Upload.vue'
+import Secret from '../pages/auth/Login.vue'
 // const _404 = require('./pages/_404.vue').default
 
 const routes = [
 
-  {
-    name: 'Home',
-    path: '/home',
-    component: Home,
-  },
-  {
-    name: 'About',
-    path: '/about',
-    component: About,
-  },
-  {
-    name: 'User',
-    path: '/user',
-    // path: '/user/:id_name?',
-    component: User,
-    // props: true,
-  },
-  {
-    name: 'Register',
-    path: '/user/create',
-    component: Register,
-  },
-  {
-    name: 'EditUser',
-    path: '/user/:id/edit',
-    component: EditUser,
-    props: true,
-  },
-  {
-    name: 'UploadImageUser',
-    path: '/user/upload/:id',
-    component: UploadImageUser,
-    props: true,
-  },
-  {
-    name: 'Profile',
-    path: '/user/:id',
-    component: Profile,
-    props: true,
-  },
-  {
-    path: '*',
-    component: _404,
-  },
+    {
+        name: 'Secret',
+        path: '/',
+        component: Secret,
+    },
+    {
+        name: 'Home',
+        path: '/home',
+        component: Home,
+    },
+    {
+        name: 'About',
+        path: '/about',
+        component: About,
+    },
+    {
+        name: 'User',
+        path: '/user',
+        // path: '/user/:id_name?',
+        component: User,
+        // props: true,
+    },
+    {
+        name: 'Register',
+        path: '/user/create',
+        component: Register,
+    },
+    {
+        name: 'EditUser',
+        path: '/user/:id/edit',
+        component: EditUser,
+        props: true,
+    },
+    {
+        name: 'UploadImageUser',
+        path: '/user/upload/:id',
+        component: UploadImageUser,
+        props: true,
+    },
+    {
+        name: 'Profile',
+        path: '/user/:id',
+        component: Profile,
+        props: true,
+    },
+    {
+        path: '*',
+        component: _404,
+    },
 ]
 
 const router = new VueRouter({
-  linkActiveClass: 'active',
-  mode: 'history',
-  routes
+    linkActiveClass: 'active',
+    mode: 'history',
+    routes
 })
 
 export default router
--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Auth::routes();
+// Auth::routes();
 
 // Route::get('home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 // Route::any('/{slug}/{slug1}', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
